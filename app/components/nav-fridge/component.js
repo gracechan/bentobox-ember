7f1b67eee3b6ac7e7ce9d9c457921a6d373212e6
gracechan <tanreichan@gmail.com>
2015-12-30 03:24:01 -0500

    Add num ingredients icon over fridge.

--- app/components/nav-fridge/component.js
+++ app/components/nav-fridge/component.js
@@ -1,10 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  selectedIngredientService: Ember.inject.service('selection-ingredient'),
+
   classNames: ['nav-fridge'],
   classNameBindings: ['isCollapsed:collapsed'],
 
   isCollapsed: true,
+
+  numIngredients: Ember.computed.alias('selectedIngredientService.getCount'),
+  ingredientList: Ember.computed.alias('selectedIngredientService.getSelectedIngredients'),
+
+  showNumIcon: Ember.computed('numIngredients', function () {
+    return this.get('numIngredients') > 0;
+  }),
 
   actions: {
     toggleContentList: function () {
