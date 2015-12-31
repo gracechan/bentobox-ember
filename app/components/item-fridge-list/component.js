3fbdc85ca922dc0681e847f40f1d503d01d34b40
gracechan <tanreichan@gmail.com>
2015-12-31 02:01:52 -0500

    Use selected service to remove ingredient when x button is clicked.

--- app/components/item-fridge-list/component.js
+++ app/components/item-fridge-list/component.js
@@ -1,5 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNames: ['item-fridge-list']
+  selectedIngredientService: Ember.inject.service('selection-ingredient'),
+  ingredient: null, // this gets passed in from above, mandatory
+
+  classNames: ['item-fridge-list'],
+
+  actions: {
+    removeIngredient: function () {
+      var id = this.get('ingredient').id;
+      this.get('selectedIngredientService').removeSelectedItem(id);
+    }
+  }
 });
